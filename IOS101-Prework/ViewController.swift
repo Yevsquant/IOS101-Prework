fce334fd731b77eabdf0841957c6bb7226770eb5
runying <67864475+Yevsquant@users.noreply.github.com>
2024-12-12 23:19:21 -0800

    changed name color

--- IOS101-Prework/ViewController.swift
+++ IOS101-Prework/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    @IBOutlet weak var label_name: UILabel!
     @IBAction func change_bg_color(_ sender: UIButton) {
         func changeColor() -> UIColor {
             let red = CGFloat.random(in: 0...1)
@@ -23,9 +23,10 @@ class ViewController: UIViewController {
             return UIColor(red: red, green: green, blue: blue, alpha: 0.5)
         }
         
-        let randomColor = changeColor()
-        view.backgroundColor = randomColor
+        let random_bg_color = changeColor()
+        let random_font_color = changeColor()
+        view.backgroundColor = random_bg_color
+        label_name.textColor = random_font_color
     }
-    
 }
 
